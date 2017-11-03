e0c3627eda560a52046698242e87ea638099eb5e
Don Lettrich <don.lettrich@gmail.com>
2017-11-03 19:26:52 -0400

    added video resource via generator and nav links

--- lib/catcasts_web/router.ex
+++ lib/catcasts_web/router.ex
@@ -17,6 +17,8 @@ defmodule CatcastsWeb.Router do
   scope "/", CatcastsWeb do
     pipe_through :browser # Use the default browser stack
 
+    resources "/videos", VideoController
+
     get "/", PageController, :index
   end
 
